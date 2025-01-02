08de3a826efa0de7466b9573d4501b6dd0c7d0f0
emmy tech <161587119+emmy4dev@users.noreply.github.com>
2025-01-02 02:48:23 +0100

    added tier features

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+        create: (context ) => myProvider(),
+        child: const MyApp(),
+    )
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -36,13 +43,153 @@ class _MyHomePageState extends State<MyHomePage> {
     return 
         const Scaffold(
             body: Center(
-              child: ScoreBoard(),
+              child: Column(
+                children: [
+                     ScoreBoard(),
+                     Expanded(
+                         child: TimerWidget(),
+
+                     ),
+
+                ],
+
+              ),
+
             ),
 
         );
   }
 }
+//provider  widget
 
+class myProvider  extends ChangeNotifier{
+  String AddDegit(int score){
+    return score  <=9 ? "0${score}" : "${score}";
+  }
+}
+
+//widget for displaying timer and timer functionality
+
+class TimerWidget extends StatefulWidget{
+  const TimerWidget({
+    super.key
+
+});
+
+  @override
+  _timerState createState() => _timerState();
+}
+class _timerState extends State<TimerWidget>{
+
+  int seconds = 0;
+  int minutes = 0;
+  late Timer _timer;
+  late TextEditingController _controllerMinutes = TextEditingController();
+  late TextEditingController _controllerSeconds = TextEditingController();
+  final myProviderInstance = myProvider();
+
+
+
+  void timers(){
+    _timer = Timer.periodic(Duration(seconds: 1), (Timer timer){
+      print("Timer ticked");
+      setState(() {
+        if (seconds > 0) {
+          seconds --;
+          _controllerSeconds.text = seconds.toString();
+          print(seconds);
+          
+        } else if (minutes > 0){
+          minutes --;
+          seconds = 59;
+          _controllerMinutes.text = minutes.toString();
+          print(minutes);
+        }
+        else if(seconds == 0 && minutes == 0){
+          _timer.cancel();
+        }
+        else{
+          _timer.cancel();
+
+        }
+
+        _controllerMinutes = TextEditingController(text: '${myProviderInstance.AddDegit(minutes)}');
+        _controllerSeconds = TextEditingController(text: '${myProviderInstance.AddDegit(seconds)}');
+
+
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+
+    _timer.cancel();
+    super.dispose();
+  }
+
+  @override
+
+
+  Widget build(BuildContext context){
+    return Center(
+      child: Column(
+        children: [
+           Row(
+            children: [
+              Flexible(child:
+              TextField(
+                controller: _controllerMinutes,
+                decoration: const InputDecoration(
+                ),
+                keyboardType: TextInputType.number,
+                onChanged:(value){
+                  setState(() {
+                    minutes = int.tryParse(value) ?? 0 ;
+                  });
+
+                },
+              ),
+
+
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Flexible(
+                child:
+              TextField(
+                controller: _controllerSeconds,
+                decoration: const InputDecoration(
+
+                ),
+                keyboardType: TextInputType.number,
+                onChanged: (value){
+                  seconds = int.tryParse(value) ?? 0 ;
+
+                },
+              ),
+              )
+
+
+            ],
+          ),
+          IconButton(
+            onPressed: (){
+              timers();
+            },
+            icon: const Icon(Icons.start),
+          ),
+        ],
+      ),
+   
+
+    );
+
+  }
+}
+
+//socre board widget;
 
 class ScoreBoard extends StatefulWidget{
    const ScoreBoard({
@@ -61,103 +208,130 @@ class  _ScoreBoardState extends State<ScoreBoard>{
   //initiazer for both teams
   int teamA = 0;
   int teamB = 0;
-  String Score = '00';
+  String selectedTeam = '';
 
-  void AddScore(String team) {
+
+  void selectedTeams( String team ){
     setState(() {
-      if(team == 'a'){
-        teamA ++;
-        String score;
-        Score = teamA <= 9 ? "0" + teamA.toString(): teamA.toString();
-        Score;
-        print(Score);
+      selectedTeam = team;
+    });
 
-      }else if(team =='b'){
-        teamB ++ ;
-        String score;
-        score = teamB <= 9 ? "0 " + teamB.toString(): teamB.toString();
-        score;
-        print(score);
+  }
+
+  void AddScore() {
+    setState(() {
+      if(selectedTeam == "a"){
+        print(teamA);
+        teamA++;
+        
+      } if(selectedTeam == "b"){
+        print(teamB);
+        teamB++;
+
+      }else {
+        return ;
       }
-
-
     });
   }
 
 
 // function to remove score
-  void RemoveScore (String team){
-    if(team == 'a'){
-      if(teamA < 0){
+  void RemoveScore (){
+    setState((){
+      if(selectedTeam=="a"){
+        if(teamA <= 0){
+          teamA;
+        }
         teamA--;
-      }else {
-        teamA;
-      }
-    }else if(team == 'b'){
-      if(teamB > 0){
+        print(teamA);
+      }if(selectedTeam == "b"){
+        if(teamB <= 0){
+          teamB;
+        }
         teamB--;
-      }else{
-        teamB;
+        print(teamB);
       }
-
-    }else{
-      team;
-    }
+    });
   }
+
+
+
   @override
   Widget build(BuildContext context) {
    return Center(
-     child: Column(
-       children: [
-         const Row(
-           crossAxisAlignment: CrossAxisAlignment.center,
-           children: [
-             Expanded(
-                 child: Column(
+     child: Consumer<myProvider>
+       (builder :
+         (context, myProvider, child)=>
+             Column(
+               children: [
+                 Row(
+                   crossAxisAlignment: CrossAxisAlignment.center,
                    children: [
-                     Text(
-                       "Team A",
+                     Expanded(
+                       child: GestureDetector(
+                           onTap : (){
+                             selectedTeams("a");
+                           },
+                           child: Column(
+                             children: [
+                               const Text(
+                                 "Team A",
+                               ),
 
+                               Text(myProvider.AddDegit(teamA)),
+                             ],
+                           )
+
+                       ),
                      ),
+                     Spacer(),
+                     Expanded(
+                         child: GestureDetector(
+                             onTap: (){
+                               selectedTeams("b");
+                               print("this is the second click");
 
-                     const Text(''),
+                             },
+                             child: Column(
+                               children: [
+                                 Text("Team B"),
+                                 Text(myProvider.AddDegit(teamB)),
+                               ],
+
+                             )
+
+                         )
+                     ),
                    ],
-                 )
-             ),
-             Spacer(),
-             Expanded(
-                 child: Column(
-                   children: [
-                     Text("Team B"),
-                     Text("00"),
-                   ],
+                 ),
+                 Padding(
+                     padding: const
+                     EdgeInsets.symmetric(horizontal: 180, vertical: 50),
+                     child: Row(
+                         children:[
+                           ElevatedButton(
+                               onPressed:(){
+                                 RemoveScore();
+                                 print("this is actually working");
+                               },
+                               child: const Text("-")
+                           ),
+                           Spacer(),
+                           ElevatedButton(
+                               onPressed:(){
+                                 AddScore();
+                                 print("this is actually working 1");
+                               },
+                               child: const Text("+")
+                           ),
 
-                 )
-             ),
-           ],
-         ),
-         Padding(
-             padding: const
-             EdgeInsets.symmetric(horizontal: 180, vertical: 50),
-             child: Row(
-           children:[
-             ElevatedButton(
-                 onPressed:(){
-                   AddScore("a");
-                 },
-                 child: const Text("-")
-            ),
-             Spacer(),
-             ElevatedButton(
-                 onPressed:(){},
-                 child: const Text("+")
-             ),
+                         ]
+                     )
+                 ),
 
-           ]
-         )
-             ),
+               ],
 
-       ],
+             )
 
      )
 
